449c89fb3c3e828788caac353223deba9807fce7
lagbullet <40629577+lagbullet@users.noreply.github.com>
2018-09-23 19:51:49 +0300

    1

---
+++ ValidationApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ValidationApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string email = Console.ReadLine();
+            Validator validator = new Validator();
+            if (validator.IsValidEmail(email))
+                Console.WriteLine("Email is valid");
+            else
+                Console.WriteLine("Email is invalid");
+            string url = Console.ReadLine();
+            if (validator.IsValidURL(url))
+                Console.WriteLine("URL is correct");
+            else
+                Console.WriteLine("URL is incorrect");
+            string filepath = Console.ReadLine();
+            if (validator.IsValidFilePath(filepath))
+                Console.WriteLine("File path is correct");
+            else
+                Console.WriteLine("File path is incorrect");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ValidationApp/Validator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace ValidationApp
+{
+    class Validator
+    {
+        bool invalid = false;
+
+        public bool IsValidURL(string url) => Regex.IsMatch(url, @"^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$", RegexOptions.IgnoreCase);
+
+        public bool IsValidFilePath(string filepath) => Regex.IsMatch(filepath, @"^(([a-zA-Z]:|\\\\\w[ \w\.]*)(\\\w[ \w\.]*|\\%[ \w\.]+%+)+|%[ \w\.]+%(\\\w[ \w\.]*|\\%[ \w\.]+%+)*)$");
+
+        public bool IsValidEmail(string strIn)
+        {
+            invalid = false;
+            if (string.IsNullOrEmpty(strIn))
+                return false;
+            try
+            {
+                strIn = Regex.Replace(strIn, @"(@)(.+)$", DomainMapper,RegexOptions.None, TimeSpan.FromMilliseconds(200));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+            if (invalid)
+                return false;
+            try
+            {
+                return Regex.IsMatch(strIn,@"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-0-9a-z]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$",RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+        }
+        private string DomainMapper(Match match)
+        {
+            IdnMapping idn = new IdnMapping();
+
+            string domainName = match.Groups[2].Value;
+            try
+            {
+                domainName = idn.GetAscii(domainName);
+            }
+            catch (ArgumentException)
+            {
+                invalid = true;
+            }
+            return match.Groups[1].Value + domainName;
+        }
+    }
+}
